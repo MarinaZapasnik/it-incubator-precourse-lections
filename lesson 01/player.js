a047b967fed745b1a7fdb199603c9e849e21c87f
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-04-02 13:48:37 +0300

    player

--- lesson 01/player.js
+++ lesson 01/player.js
@@ -3,78 +3,64 @@ const playList = {
     playListInfo: {
         title: 'Hip-hop hits',
         coverImgUrl: './cover.avif',
-        totalInfo: {
-            totalTrackCount: 4,
-            totalTrackDurationInSec: 733,
-        }
+        
     },
     tracks: [
         {
             trackId: '01',
             trackTitle: 'Rap God',
             artistName: 'Eminem',
-            trackCoverImgUrl: './eminem.jpg',
+            trackCoverImgUrl: "C:\Users\iZiPC.by\it-incubator precourse lections\lesson 01\eminem.jpg",
             trackFileUrl: './eminem_-_rap_god_(muztune.me).mp3',
         },
         {
             trackId: '02',
             trackTitle: 'In Da Club',
             artistName: '50cent',
-            trackCoverImgUrl: './50cent.jpg',
+            trackCoverImgUrl: "C:\Users\iZiPC.by\it-incubator precourse lections\lesson 01\50cent.jpg",
             trackFileUrl: './50cent_-_in_da_club_(muztune.me).mp3',
-        },
+        }
             ],
 }
 
-// const playListTitleElement = document.createElement('h1');
-// playListTitleElement.innerText = playList.playListInfo.title;
-// document.body.append(playListTitleElement);
+const playListTitleElement = document.createElement('h1');
+playListTitleElement.innerText = playList.playListInfo.title;
+document.body.append(playListTitleElement);
 
+const playListCoverElement = document.createElement('img');
+playListCoverElement.src = playList.playListInfo.coverImgUrl;
+playListCoverElement.style.width = '150px';
+playListCoverElement.style.height = '150px';
+document.body.append(playListCoverElement);
 
+const trackListElement = document.createElement('ul');
 
-function renderPlayList(anyPlayList) {
+for (let i = 0; playList.tracks.length; i++) {
+    const trackElement = document.createElement('li');
 
-    renderPlayListHeader(anyPlayList.playListInfo)
+    const trackCoverElement = document.createElement('img');
+    trackCoverElement.src = playList.tracks[i].trackCoverImgUrl;
+    
+    trackElement.append(trackCoverElement);
+    trackElement.append(playList.tracks[i].artistName + ': ' +  playList.tracks[i].trackTitle);
+    trackListElement.append(trackElement);
 
-    renderTracksList(anyPlayList.tracks)
-
-}
-function renderPlayListHeader(anyPlayListInfo) {
-    const playListTitleElement = document.createElement('h1');
-    playListTitleElement.innerText = anyPlayListInfo.title;
-    document.body.append(playListTitleElement);
-
-    const playListCoverElement = document.createElement('img');
-    playListCoverElement.style.width = '50px';
-    playListCoverElement.style.height = '50px'
-    playListCoverElement.src = anyPlayListInfo.coverImgUrl;
-    document.body.append(playListCoverElement);
-}
-
-function renderTracksList(anyTracks) {
-const trackListElement = document.createElement("ul");
-    for (let i = 0; i < anyTracks.length; i++) {
-       
    
-    }
-        document.body.append(trackListElement);
 }
+document.body.append(trackListElement);
 
-function createTrack (anyTrack) {
-    
-        const trackElement = document.createElement('li');
-        const trackCoverElement = document.createElement('img');
-        trackCoverElement.style.height = '50px';
-        trackCoverElement.style.width = '50px'
-        trackCoverElement.src = anyTrack.trackCoverImgUrl;
-    
-        trackElement.append(trackCoverElement);
-        trackElement.append(anyTracsList.artistName + ": " + anyTracsList.trackTitle)
-    
-        trackListElement.append(trackElement);
-    }
 
-renderPlayList(playList)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
