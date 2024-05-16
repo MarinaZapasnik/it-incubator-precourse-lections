93e8c569c65d1b6eb6f478904b5fd7d8db7d8fa2
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-05-16 13:15:26 +0300

    im glad done

--- lesson 01 second time/player.js
+++ lesson 01 second time/player.js
@@ -24,26 +24,108 @@ const playlist = {
 
 }
 
-const playlistTitleElement = document.createElement('h1');
-playlistTitleElement.innerText = playlist.playlistInfo.title;
-document.body.append(playlistTitleElement);
+// const playlistTitleElement = document.createElement('h1');
+// playlistTitleElement.innerText = playlist.playlistInfo.title;
+// document.body.append(playlistTitleElement);
 
-const playlistCoverImgElement = document.createElement('img');
-playlistCoverImgElement.src = playlist.playlistInfo.coverImg;
-playlistCoverImgElement.style.height = '100px';
-playlistCoverImgElement.style.width = '100px';
-document.body.append(playlistCoverImgElement);
+// const playlistCoverImgElement = document.createElement('img');
+// playlistCoverImgElement.src = playlist.playlistInfo.coverImg;
+// playlistCoverImgElement.style.height = '100px';
+// playlistCoverImgElement.style.width = '100px';
+// document.body.append(playlistCoverImgElement);
 
-const trackListElement = document.createElement('ul');
-for (let i = 0; i < playlist.tracks.length; i++) {
+// const trackListElement = document.createElement('ul');
+// for (let i = 0; i < playlist.tracks.length; i++) {
 
-    
-    const trackElement = document.createElement('li');
-    const trackCoverelement = document.createElement('img');
-    trackCoverelement.src = playlist.tracks[i].trackCoverImgUrl;
-    const trackFileElement = document.createElement('audio');
-    trackFileElement.src = playlist.tracks[i].trackFileUrl
+
+//     const trackElement = document.createElement('li');
+//     const trackCoverElement = document.createElement('img');
+//     trackCoverElement.src = playlist.tracks[i].trackCoverImgUrl;
+//     trackCoverElement.style.width = '50px';
+//     trackCoverElement.style.height = '50px';
+//     const trackFileElement = document.createElement('audio');
+//     trackFileElement.controls = true;
+//     trackFileElement.src = playlist.tracks[i].trackFileUrl;
+//     trackElement.append(
+//         trackCoverElement,
+//         playlist.tracks[i].artistName + ' ',
+//         '"' + playlist.tracks[i].trackTitle + '" ',
+//         trackFileElement
+//     )
+//     trackListElement.append(trackElement);
+// }
+
+// document.body.append(trackListElement)
+
+
+function renderPlayList(anyPlayList) {
+    renderPlayListHeader(anyPlayList.playlistInfo)
+    renderTracksList(anyPlayList.tracks)
 }
+function renderPlayListHeader(anyPlayListInfo) {
+
+    const playlistTitleElement = document.createElement('h1');
+    playlistTitleElement.innerText = anyPlayListInfo.title;
+    document.body.append(playlistTitleElement);
+
+    const playlistCoverImgElement = document.createElement('img');
+    playlistCoverImgElement.src = anyPlayListInfo.coverImg;
+    playlistCoverImgElement.style.height = '100px';
+    playlistCoverImgElement.style.width = '100px';
+    document.body.append(playlistCoverImgElement);
+
+}
+
+function renderTracksList(anyTtracks) {
+    
+    const trackListElement = document.createElement('ul');
+    for (let i = 0; i < anyTtracks.length; i++) {
+
+        
+       const trackElement = renderTrack(anyTtracks[i]);
+
+        // const trackElement = document.createElement('li');
+        // const trackCoverElement = document.createElement('img');
+        // trackCoverElement.src = anyTtracks[i].trackCoverImgUrl;
+        // trackCoverElement.style.width = '50px';
+        // trackCoverElement.style.height = '50px';
+        // const trackFileElement = document.createElement('audio');
+        // trackFileElement.controls = true;
+        // trackFileElement.src = anyTtracks[i].trackFileUrl;
+        // trackElement.append(
+        //     trackCoverElement,
+        //     anyTtracks[i].artistName + ' ',
+        //     '"' + anyTtracks[i].trackTitle + '" ',
+        //     trackFileElement
+        // )
+        trackListElement.append(trackElement);
+    }
+
+    document.body.append(trackListElement);
+
+}
+
+function renderTrack(anyTrack) {
+
+    const trackElement = document.createElement('li');
+    const trackCoverElement = document.createElement('img');
+    trackCoverElement.src = anyTrack.trackCoverImgUrl;
+    trackCoverElement.style.width = '50px';
+    trackCoverElement.style.height = '50px';
+    const trackFileElement = document.createElement('audio');
+    trackFileElement.controls = true;
+    trackFileElement.src = anyTrack.trackFileUrl;
+    trackElement.append(
+        trackCoverElement,
+        anyTrack.artistName + ': ',
+        '"' + anyTrack.trackTitle + '" ',
+        trackFileElement
+    )
+    return trackElement;
+}
+
+renderPlayList(playlist);
+
 
 
 
