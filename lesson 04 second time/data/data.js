bc03dfe86696538f44a88e02f63be675f8f4c050
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-05-23 13:46:54 +0300

    first commit

---
+++ lesson 04 second time/data/data.js
@@ -0,0 +1,28 @@
+export const data = {
+    todolist: {
+        id: crypto.randomUUID(),
+        title: 'Todolist',
+        tasks: [
+            {
+                id: crypto.randomUUID(),
+                title: 'Learn HTML'
+            },
+            {
+                id: crypto.randomUUID(),
+                title: 'Learn CSS'
+            },
+            {
+                id: crypto.randomUUID(),
+                title: 'Learn JS'
+            },
+            {
+                id: crypto.randomUUID(),
+                title: 'Learn React'
+            },
+        ],
+        addNewTaskDialog: {
+            isOpen: false
+        }
+    }
+}
+
